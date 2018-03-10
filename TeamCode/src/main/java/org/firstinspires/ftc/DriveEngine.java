d5dd17186b4125d2e264c030568dbcd41c314b8e
Jordan <martinj1@gmail.com>
2018-03-10 09:16:31 -0500

    turn to heading ready to test

--- TeamCode/src/main/java/org/firstinspires/ftc/DriveEngine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/DriveEngine.java
@@ -2,8 +2,10 @@ package org.firstinspires.ftc;
 
 import android.util.Log;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 /**
@@ -13,12 +15,19 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class DriveEngine {
     DcMotor leftMotor;
     DcMotor rightMotor;
+    GyroSensor orientation;
+    PIDController turnController;
     HardwareMap hardwareMap;
     private final int TICKS_PER_REV = 560;
     private final double DRIVE_WHEEL_DIAMETER_INCHES = 4.0;
+    private final double HEADING_THRESHOLD = 2.0;
+    private final double TURN_KP = 0.05; // gain * error
+    private final double TURN_KI = 0.01; // gain * error * time(some decimal of seconds)
+    private final double TURN_KD = 0;
 
     public DriveEngine(HardwareMap hw){
         hardwareMap = hw;
+        turnController = new PIDController(TURN_KP, TURN_KI, TURN_KD);
         leftMotor = hardwareMap.dcMotor.get("leftMotor");
         rightMotor = hardwareMap.dcMotor.get("rightMotor");
         leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -66,10 +75,24 @@ public class DriveEngine {
         leftMotor.setPower(power);
         rightMotor.setPower(power);
     }
-//    public void turn(double speed, double heading){
-//        double distFromHeading = bloop-heading;
-//
-//    }
+    public void turn(double heading, LinearOpMode mode){
+        turnController.setSp(0);
+        double power;
+        double distFromHeading = heading - orientation.getHeading();
+        // makes negative counter-clockwise and positive clockwise
+        if(distFromHeading > 180) distFromHeading -= 360;
+        else if(distFromHeading < -180) distFromHeading += 360;
+
+        while (mode.opModeIsActive() && Math.abs(distFromHeading) > HEADING_THRESHOLD){
+            power = turnController.calculatePID(distFromHeading);
+            turn(power);
+            mode.sleep(5);
+            distFromHeading = heading - orientation.getHeading();
+            if(distFromHeading > 180) distFromHeading -= 360;
+            else if(distFromHeading < -180) distFromHeading += 360;
+        }
+        brake();
+    }
     public void kill(){
         brake();
     }
